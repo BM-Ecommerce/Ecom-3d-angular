2d40160a8f470668b2756b13690d9b4007ab7ec6
Praveen <praveen@blindmatrix.com>
2025-11-26 20:31:25 +0530

    tess

--- karma.conf.js
+++ karma.conf.js
@@ -36,24 +36,16 @@ module.exports = function (config) {
 customLaunchers: {
   ChromeHeadlessNoSandbox: {
     base: 'ChromeHeadless',
-    flags: [
-      '--no-sandbox',
-      '--disable-gpu',
-      '--disable-dev-shm-usage',
-      '--remote-debugging-port=9222'
-    ]
+    flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage']
   }
 },
 browsers: ['ChromeHeadlessNoSandbox'],
 singleRun: true,
-
-    
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadlessNoSandbox'],
     singleRun: true,
     restartOnFileChange: true
   });
