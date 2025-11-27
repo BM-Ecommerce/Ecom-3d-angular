f56c4014c22f03b538a07d184c39cf5188bb7fde
Praveen <praveen@blindmatrix.com>
2025-11-27 13:11:37 +0530

    final pipeline

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,3 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -12,25 +9,24 @@ module.exports = function (config) {
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+
     client: {
       jasmine: {},
       clearContext: false
     },
-    jasmineHtmlReporter: {
-      suppressAll: true
-    },
+
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/visualization'),
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' }
+        { type: 'text-summary' },
+        { type: 'lcovonly' }
       ]
     },
 
-    // IMPORTANT: custom launcher
     customLaunchers: {
-      ChromeHeadlessNoSandbox: {
+      ChromeHeadlessCI: {
         base: 'ChromeHeadless',
         flags: [
           '--no-sandbox',
@@ -41,33 +37,12 @@ module.exports = function (config) {
       }
     },
 
-    // IMPORTANT: tell Karma to use this launcher
-    browsers: ['ChromeHeadlessNoSandbox'],
-
+    browsers: ['ChromeHeadlessCI'],
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    singleRun: true,
-    restartOnFileChange: true
-  });
-};
-
-
-module.exports = function (config) {
-  config.set({
-    browsers: ['ChromeHeadlessCI'],
-    customLaunchers: {
-      ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
-        flags: [
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-          '--disable-gpu',
-          '--disable-dev-shm-usage'
-        ]
-      }
-    }
+    singleRun: true
   });
 };
