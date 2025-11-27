055774949c0e27297a1449337dc0976f8ddb0b9e
Praveen <praveen@blindmatrix.com>
2025-11-27 12:53:47 +0530

    karma file addition

--- karma.conf.js
+++ karma.conf.js
@@ -13,16 +13,11 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      jasmine: {},
+      clearContext: false
     },
     jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      suppressAll: true
     },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/visualization'),
@@ -31,27 +26,30 @@ module.exports = function (config) {
         { type: 'html' },
         { type: 'text-summary' }
       ]
-      },
-      
-customLaunchers: {
-  ChromeHeadlessNoSandbox: {
-    base: 'ChromeHeadless',
-    flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage']
-  }
+    },
 
-},    
+    // IMPORTANT: custom launcher
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-gpu',
+          '--disable-dev-shm-usage',
+          '--remote-debugging-port=9222'
+        ]
+      }
+    },
+
+    // IMPORTANT: tell Karma to use this launcher
+    browsers: ['ChromeHeadlessNoSandbox'],
+
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
+    autoWatch: false,
     singleRun: true,
-    restartOnFileChange: true,
-    browsers: ['ChromeHeadlessNoSandbox'],
-
+    restartOnFileChange: true
   });
 };
-
-
-
-
