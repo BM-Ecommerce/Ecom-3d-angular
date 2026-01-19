31a4f036c8506fc062ceacd2707a1cfdb2c91413
Vikas <vikas@blindmatrix.co.uk>
2026-01-19 14:08:52 +0530

    DISCOUNTBUYBLINDS changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,10 +4,10 @@
 // DO NOT HARDCODE REAL CREDENTIALS HERE.
 export const environment = {
   production: true,
-  apiUrl: 'https://blindmatrix.software',
-  apiKey: '0d5b2abe-d707-4eb3-a7cb-3f05a5e5d3fb',
-  apiName: 'ECOMMERCE',
-  site: 'https://blindssoftware.com',
+  apiUrl: 'https://us.blindmatrix.software',
+  apiKey: 'e1d4b939-75de-46f7-b02d-2f5db6512e70',
+  apiName: 'DISCOUNTBUYBLINDS',
+  site: 'https://discountbuyblinds.blindssoftware.com',
   loaderMode: 'topbar' as 'overlay' | 'topbar',
   loaderEnabled: true
 };
